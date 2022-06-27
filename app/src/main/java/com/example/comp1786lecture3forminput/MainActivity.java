dc4b7cb78d8e7c7faf272904380372f3c0b7d805
Vinh Hoang <64019402+vinhhn3@users.noreply.github.com>
2022-06-27 21:30:53 +0700

    Display DatePicker

--- app/src/main/java/com/example/comp1786lecture3forminput/MainActivity.java
+++ app/src/main/java/com/example/comp1786lecture3forminput/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.comp1786lecture3forminput;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -23,5 +25,10 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, workStatusArray);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sp.setAdapter((dataAdapter));
+    }
+
+    public void showDatePickerDialog(View v){
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
