584ad784ac410d4923d406ad834b35ae3ac492cb
Vinh Hoang <64019402+vinhhn3@users.noreply.github.com>
2022-06-27 22:00:16 +0700

    Code to Display Alert

--- app/src/main/java/com/example/comp1786lecture3forminput/MainActivity.java
+++ app/src/main/java/com/example/comp1786lecture3forminput/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.comp1786lecture3forminput;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -18,16 +23,30 @@ public class MainActivity extends AppCompatActivity {
             "Unemployment"
     };
 
-    private Spinner sp;
+    private Spinner spinnerWorkStatus;
+    private EditText nameInput;
+    private EditText emailInput;
+    private EditText phoneInput;
+    private Button saveButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sp = (Spinner) findViewById(R.id.spinnerWorkStatus);
+        spinnerWorkStatus = (Spinner) findViewById(R.id.spinnerWorkStatus);
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, workStatusArray);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        sp.setAdapter((dataAdapter));
+        spinnerWorkStatus.setAdapter((dataAdapter));
+
+        saveButton = findViewById(R.id.buttonSave);
+
+        saveButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                getInputs();
+            }
+        });
     }
 
     public void showDatePickerDialog(View v){
@@ -39,4 +58,35 @@ public class MainActivity extends AppCompatActivity {
         TextView dobText = (TextView) findViewById(R.id.inputDob);
         dobText.setText(dob.toString());
     }
+
+    private void getInputs(){
+        nameInput = findViewById(R.id.inputName);
+        emailInput = findViewById(R.id.inputEmail);
+        phoneInput = findViewById(R.id.inputPhone);
+        spinnerWorkStatus = findViewById(R.id.spinnerWorkStatus);
+
+        String strName = nameInput.getText().toString();
+        String strPhone = phoneInput.getText().toString();
+        String strEmail = emailInput.getText().toString();
+        String strWork = spinnerWorkStatus.getSelectedItem().toString();
+
+        displayNextAlert(strName, strPhone, strEmail, strWork);
+    }
+
+    private void displayNextAlert(String strName, String strPhone, String strEmail, String strWork){
+        new AlertDialog.Builder(this).setTitle("Details entered").setMessage("Details entered: " +
+                "\n" + strName +
+                "\n" + strPhone +
+                "\n" + strEmail +
+                "\n" + strWork
+        ).setNeutralButton("Back", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        }).show();
+    }
+
+
+
 }
