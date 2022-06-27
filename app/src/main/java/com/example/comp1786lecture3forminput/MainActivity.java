3701282f34fdb9a50d0762905ed07ca0a108241d
Vinh Hoang <64019402+vinhhn3@users.noreply.github.com>
2022-06-27 21:18:35 +0700

    Popuplate Spinner through Code

--- app/src/main/java/com/example/comp1786lecture3forminput/MainActivity.java
+++ app/src/main/java/com/example/comp1786lecture3forminput/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.comp1786lecture3forminput;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] workStatusArray = {
+            "Full Time",
+            "Part Time",
+            "Unemployment"
+    };
 
+    private Spinner sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sp = (Spinner) findViewById(R.id.spinnerWorkStatus);
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, workStatusArray);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp.setAdapter((dataAdapter));
     }
 }
